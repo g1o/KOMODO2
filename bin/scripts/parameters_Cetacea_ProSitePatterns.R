c11f473c94b9e482cc46df259b475307517611e0
franciscolobo <franciscolobo@gmail.com>
2019-07-01 20:31:55 -0300

    Delete parameters_Cetacea_ProSitePatterns.R

--- bin/scripts/parameters_Cetacea_ProSitePatterns.R
+++
@@ -1,8 +0,0 @@
-KOMODO2 <- list(type = "correlation",
-                output.dir = "/home/chico/projects/KOMODO2/validation/Cetartiodactyla_weight/results/KOMODO2/ProSitePatterns_ceta/",
-                dataset.info = "metadata/Cetacea/metadata_cetacea_ProSitePatterns.txt",                x.column = 2,
-                denominator.column = 3,
-                ontology = "other",
-                dict.path = "/home/chico/projects/KOMODO2/validation/Cetartiodactyla_weight/data/dics/Cetacea/ProSitePatterns.dic",
-                column = "ProSitePatterns",
-                cores = 50)
