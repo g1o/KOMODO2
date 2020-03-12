63ebcfbee324d36e8a985d8173b2984a6980dda8
fcampelo <fcampelo@ufmg.br>
2020-03-12 23:53:32 +0000

    Working on documentation

--- data_folder/parameters_validation/parameters_Pfam_less_H_sapiens.R
+++ data_folder/parameters_validation/parameters_Pfam_less_H_sapiens.R
@@ -1,28 +1,28 @@
-#This is a sample parameter file for KOMODO2. Most variables are self-explanatory, and a formal definition of each can be found in KOMODO2 documentation.
+# This is a sample parameter file for KOMODO2.
+# NOTICE: It assumes that the relevant data files (including this one) were
+# retrieved using KOMODO2::retrieve_data_files("./data_folder").
+# Change the target folder/file paths if needed
 
-KOMODO2 <- list(annotation_files_dir = "../../data/Pfam/", #Directory where annotation files are located. If not provided, file described in variable "dataset.info" should contain absolute paths to annotation files
-                output.dir = "../../results/Pfam_less_H_sapiens/", #output directory for results
+annotation.files.dir = "./data_folder/Pfam/" #Directory where annotation files are located. If not provided file described in variable "dataset.info" should contain absolute paths to annotation files
 
-                dataset.info = "../../data/metadata/Pfam_metadata_less_H_sapiens.txt", #genome metadata file, it should contain at least, for each genome: 1) path for annotation data; 2) phenotype data (numeric); 3) normalization data (numeric)
+output.dir = "./results/Pfam_less_H_sapiens/" #output directory for results
 
-                x.column = 2, #which column contains phenotype data?
+dataset.info = "./data_folder/metadata/Pfam_metadata_less_H_sapiens.txt" #genome metadata file it should contain at least for each genome: 1) path for annotation data; 2) phenotype data (numeric); 3) normalization data (numeric)
 
-                ontology = "other", #which dictionary data type?
+x.column = 2 #which column contains phenotype data?
 
-                dict.path = "../../data/dics/Pfam_less_H_sapiens.dic", #file for dictionary file (two-column file containing annotation IDs and their descriptions, not needed for GO
+ontology = "other" #which dictionary data type?
 
-                column = "Pfam", #which column in annotation file should be used (column name)
-                denominator.column = 4, #which column contains normalization data (numeric)
-                tree_path = "../../data/trees/tree_genome_IDs_less_H_sapiens.nwk", #path for tree file
+dict.path = "./data_folder/dics/Pfam_less_H_sapiens.dic" #file for dictionary file (two-column file containing annotation IDs and their descriptions not needed for GO
 
-                tree_type = "newick", #tree file type (either "nexus" or "newick", case-sensitive)
-                cores = 4, #how many cores to use?
+column = "Pfam" #which column in annotation file should be used (column name)
 
+denominator.column = 4 #which column contains normalization data (numeric)
 
-                #ADVANCED PARAMETERS (not really...)
+tree.path = "./data_folder/trees/tree_genome_IDs_less_H_sapiens.nwk" #path for tree file
 
-                #These parameters are basically to tell KOMODO2 how much graphical output it should produce. We configure it to generate plots only for annotation terms with corrected q-values for phylogenetically independent contrasts smaller than 0.5. 
-                
-                linear_model_cutoff = 0.5
+tree.type = "newick" #tree file type (either "nexus" or "newick" case-sensitive)
 
-                )
+# cores = 4 #how many cores to use?
+
+linear.model.cutoff = 0.5 # basically to tell KOMODO2 how much graphical output it should produce. We configure it to generate plots only for annotation terms with corrected q-values for phylogenetically independent contrasts smaller than 0.5.
--- data_folder/parameters_validation/parameters_gene2GO_standard.R
+++ data_folder/parameters_validation/parameters_gene2GO_standard.R
@@ -1,28 +1,28 @@
-#This is a sample parameter file for KOMODO2. Most variables are self-explanatory, and a formal definition of each can be found in KOMODO2 documentation.
+# This is a sample parameter file for KOMODO2.
+# NOTICE: It assumes that the relevant data files (including this one) were
+# retrieved using KOMODO2::retrieve_data_files("./data_folder").
+# Change the target folder/file paths below if needed
 
-KOMODO2 <- list(annotation_files_dir = "../../data/gene2GO/", #Directory where annotation files are located. If not provided, file described in variable "dataset.info" should contain absolute paths to annotation files
-                output.dir = "../../results/gene2GO_standard", #output directory for results
+annotation.files.dir = "./data_folder/gene2GO/" #Directory where annotation files are located. If not provided file described in variable "dataset.info" should contain absolute paths to annotation files
 
-                dataset.info = "../../data/metadata/GO_metadata_standard.txt", #genome metadata file, it should contain at least, for each genome: 1) path for annotation data; 2) phenotype data (numeric); 3) normalization data (numeric)
+output.dir = "./results/gene2GO_standard" #output directory for results
 
-                x.column = 2, #which column contains phenotype data?
+dataset.info = "./data_folder/metadata/GO_metadata_standard.txt" #genome metadata file it should contain at least for each genome: 1) path for annotation data; 2) phenotype data (numeric); 3) normalization data (numeric)
 
-                ontology = "GO", #which dictionary data type?
+x.column = 2 #which column contains phenotype data?
 
-                dict.path = "", #file for dictionary file (two-column file containing annotation IDs and their descriptions, not needed for GO
+ontology = "GO" #which dictionary data type?
 
-                column = "GO", #which column in annotation file should be used (column name)
-                denominator.column = "", #which column contains normalization data (numeric)
-                tree_path = "../../data/trees/tree_genome_IDs.nwk", #path for tree file
+dict.path = "" #file for dictionary file (two-column file containing annotation IDs and their descriptions not needed for GO
 
-                tree_type = "newick", #tree file type (either "nexus" or "newick", case-sensitive)
-                cores = 4, #how many cores to use?
+column = "GO" #which column in annotation file should be used (column name)
 
+denominator.column = "" #which column contains normalization data (numeric)
 
-                #ADVANCED PARAMETERS (not really...)
+tree_path = "./data_folder/trees/tree_genome_IDs.nwk" #path for tree file
 
-                #These parameters are basically to tell KOMODO2 how much graphical output it should produce. We configure it to generate plots only for annotation terms with corrected q-values for phylogenetically independent contrasts smaller than 0.5. 
-                
-                linear_model_cutoff = 0.5
+tree_type = "newick" #tree file type (either "nexus" or "newick" case-sensitive)
 
-                )
+# cores = 4 #how many cores to use?
+
+linear_model_cutoff = 0.5 # basically to tell KOMODO2 how much graphical output it should produce. We configure it to generate plots only for annotation terms with corrected q-values for phylogenetically independent contrasts smaller than 0.5.
