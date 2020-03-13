cb17da61976b304827583e1b6de1d019cac0cfaf
fcampelo <fcampelo@ufmg.br>
2020-03-13 21:47:47 +0000

    Fix error in read_komodo2_file()

--- R/read_komodo2_file.R
+++ R/read_komodo2_file.R
@@ -7,6 +7,15 @@ read_komodo2_file <- function(file.path){
                           stringsAsFactors = FALSE)
 
   outlist <- as.list(df$Value)
+
+  # Coerce numeric values to "numeric"
+  outlist <- lapply(outlist,
+                    function(v){
+                      if(!is.na(suppressWarnings(as.numeric(v)))) {
+                        return(as.numeric(v))
+                      } else return(v)
+                    })
+
   names(outlist) <- as.character(df$Key)
   class(outlist) <- c("KOMODO2", "list")
   return(outlist)
---
+++ results/test_examples.R
@@ -0,0 +1,13 @@
+# Run the package with all test scripts available in the
+# folder data_files/parameters_validation
+
+defs.list <- dir("./data_files/parameters_validation",
+                 pattern = "parameters_",
+                 full.names = TRUE)
+
+for (i in seq_along(defs.list)){
+  cat("\n\n-----\nRunning file", i, "\n")
+  defs <- run_KOMODO2(defs.list[i], cores = 3)
+  fn   <- paste0(defs$output.dir, "final_defs.rds")
+  saveRDS(defs, fn)
+}
