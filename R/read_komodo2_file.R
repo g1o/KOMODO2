92383a713edc2333007333ea24bccb67c7c6c72a
fcampelo <fcampelo@ufmg.br>
2020-03-13 01:04:30 +0000

    Updating (for package testing).

---
+++ R/read_komodo2_file.R
@@ -0,0 +1,13 @@
+# Read a komodo2 list from a key-value file.
+read_komodo2_file <- function(file.path){
+  df <- utils::read.table(file = file.path, header = FALSE,
+                          strip.white = TRUE,
+                          comment.char = "#", sep = '=' ,
+                          col.names = c('Key', 'Value'),
+                          stringsAsFactors = FALSE)
+
+  outlist <- as.list(df$Value)
+  names(outlist) <- as.character(df$Key)
+  class(outlist) <- c("KOMODO2", "list")
+  return(outlist)
+}
