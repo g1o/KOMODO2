050a6f5f5782145ae8138e517bad2cc3a32f4ea5
fcampelo <fcampelo@ufmg.br>
2020-03-17 13:15:35 +0000

    Some tests

---
+++ results/test_gene2go_pan_proxy.R
@@ -0,0 +1,11 @@
+# Reinstall first:
+devtools::install_github("fcampelo/KOMODO2")
+
+library(KOMODO2)
+
+# Retrieve fresh data files:
+retrieve_data_files("./data_folder")
+
+# Run the example:
+defs <- "./data_folder/parameters_validation/parameters_gene2GO_Pan_proxy.txt"
+defs <- run_KOMODO2(defs, cores = parallel::detectCores() - 1)
